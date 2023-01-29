31e81e457c9e9de34131a6118550c822d78789e1
Sam083 <samarthg8383@gmail.com>
2023-01-30 00:51:05 +0530

    adding

---
+++ $RECYCLE.BIN/S-1-5-21-1999688683-718526582-1104049511-1002/$RLDI3N2/app/src/main/java/com/example/navigationframework/loginFragment.kt
@@ -0,0 +1,24 @@
+package com.example.navigationframework
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class loginFragment : Fragment() {
+
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragment_login_framework, container, false)
+        val textR
+    }
+
+
+}
